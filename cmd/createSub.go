b8c3de9172935f03a25060f40a8562617b4c27d1
Arthur EICHELBERGER <arthur.eichelberger@lydia-app.com>
2021-07-11 16:53:01 +0200

    enhance(subber): enhance create sub command

--- cmd/createSub.go
+++ cmd/createSub.go
@@ -2,10 +2,12 @@ package cmd
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	ps "cloud.google.com/go/pubsub"
 	"github.com/arthureichelberger/subber/pkg/pubsub"
+	"github.com/arthureichelberger/subber/service"
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -14,8 +16,31 @@ import (
 var createSubCmd = &cobra.Command{
 	Use: "createSub",
 	Run: func(cmd *cobra.Command, args []string) {
-		subName := args[0]
-		topicName := args[1]
+		subName, err := service.NewPrompt("Please enter the subscriptionName", func(value string) error {
+			if len(value) == 0 {
+				return errors.New("sub name cannot be null")
+			}
+
+			return nil
+		})
+
+		if err != nil {
+			pterm.Error.Println(err.Error())
+			return
+		}
+
+		topicName, err := service.NewPrompt("Please enter the topicName", func(value string) error {
+			if len(value) == 0 {
+				return errors.New("topic name cannot be null")
+			}
+
+			return nil
+		})
+
+		if err != nil {
+			pterm.Error.Println(err.Error())
+			return
+		}
 
 		ctx := context.Background()
 		client, err := pubsub.NewPubsubClient(ctx, fmt.Sprintf("%v", viper.Get("PUBSUB_PROJECT_ID")))
@@ -31,13 +56,12 @@ var createSubCmd = &cobra.Command{
 			Topic: topic,
 		})
 		if err != nil {
-			pterm.Error.Printfln("Could not create subscription %s.", subName)
+			pterm.Error.Printfln("Could not create subscription %s. (%s)", subName, err.Error())
 			return
 		}
 
-		pterm.Success.Printfln("Subscription %s created.\n", subName)
+		pterm.Success.Printfln("Subscription %s created.", subName)
 	},
-	Args: cobra.ExactArgs(2),
 }
 
 func init() {
